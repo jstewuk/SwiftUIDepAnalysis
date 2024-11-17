b97048df8b284040aab148f13e894fbb53832a91
Jim Stewart <jim@stewartstuff.com>
2024-11-17 14:44:05 -0600

    Initial Commit

---
+++ SwiftUIDepAnalysis/SwiftUIDepAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDepAnalysisApp.swift
+//  SwiftUIDepAnalysis
+//
+//  Created by James Stewart on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDepAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

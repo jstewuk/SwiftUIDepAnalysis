432ccdf537472d5a29611cb7dcaa98871e607e27
Jim Stewart <jim@stewartstuff.com>
2024-11-17 16:21:12 -0600

    Even with separate Subview struct, still seeing cascading render/evals

--- SwiftUIDepAnalysis/ContentView.swift
+++ SwiftUIDepAnalysis/ContentView.swift
@@ -16,39 +16,10 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
                 .background(.randomColor)
                 .debugPrint("Image evaluated")
-            VStack {
-                Text(model.sv1text)
-                Button("Increment SV1") {
-                    model.updateSV1()
-                }
-                .background(.randomColor)
-                .debugPrint("SV1 VStack evaluated")
-            }
-            VStack {
-                Text(model.sv2text)
-                Button("Increment SV2") {
-                    model.updateSV2()
-                }
-                .background(.randomColor)
-                .debugPrint("SV2 VStack evaluated")
-            }
-            VStack {
-                Text(model.sv3text)
-                Button("Increment SV3") {
-                    model.updateSV3()
-                }
-                .background(.randomColor)
-                .debugPrint("SV3 VStack evaluated")
-            }
-            VStack {
-                Text(model.sv4text)
-                Button("Increment SV4") {
-                    model.updateSV4()
-                }
-                .background(.randomColor)
-                .debugPrint("SV4 VStack evaluated")
-            }
-
+            Subview(model: model, item: .SV1)
+            Subview(model: model, item: .SV2)
+            Subview(model: model, item: .SV3)
+            Subview(model: model, item: .SV4)
         }
         .debugPrint("body evaluated")
         .background(.randomColor)
@@ -56,35 +27,11 @@ struct ContentView: View {
     }
 }
 
-extension ShapeStyle where Self == Color {
-    public static var randomColor: some ShapeStyle {
-        Color(
-            red: Double.random(in: 0.0 ... 1.0),
-            green: Double.random(in: 0.0 ... 1.0),
-            blue: Double.random(in: 0.0 ... 1.0),
-            opacity: Double.random(in: 0.0 ... 1.0)
-            )
-    }
-}
-
-extension View {
-    func debugPrint(_ vars: Any...) -> some View {
-        for v in vars { print(v) }
-        return self
-    }
-    
-    func debugPrint() -> some View {
-        print(Self.self)
-        return self
-    }
-    
-    func debugPrint(_ string: String) -> some View {
-        print(string)
-        return self
-    }
-}
-
 final class Model: ObservableObject {
+    enum SVItem: String {
+        case SV1, SV2, SV3, SV4
+    }
+    
     @Published var sv1text = "SV1"
     @Published var sv2text = "SV2"
     @Published var sv3text = "SV3"
@@ -95,24 +42,21 @@ final class Model: ObservableObject {
     private var sv3Value: Int = 0
     private var sv4Value: Int = 0
     
-    func updateSV1() {
-        sv1Value += 1
-        sv1text = "SV1: \(sv1Value)"
-    }
-    
-    func updateSV2() {
-        sv2Value += 1
-        sv2text = "SV2: \(sv2Value)"
-    }
-    
-    func updateSV3() {
-        sv3Value += 1
-        sv3text = "SV3: \(sv3Value)"
-    }
-    
-    func updateSV4() {
-        sv4Value += 1
-        sv4text = "SV4: \(sv4Value)"
+    func update(_ svItem: SVItem) {
+        switch svItem {
+            case .SV1:
+            sv1Value += 1
+            sv1text = "SV1: \(sv1Value)"
+        case .SV2:
+            sv2Value += 1
+            sv2text = "SV2: \(sv2Value)"
+        case .SV3:
+            sv3Value += 1
+            sv3text = "SV3: \(sv3Value)"
+        case .SV4:
+            sv4Value += 1
+            sv4text = "SV4: \(sv4Value)"
+        }
     }
 }
 

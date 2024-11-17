ca63479087c62442953c648aa5fbe2ef45e6ceea
Jim Stewart <jim@stewartstuff.com>
2024-11-17 15:34:28 -0600

    Verify that changing any published value in an observable drives a complete rerender -- even when a view in not dependent on any of the published values -- eg Image

--- SwiftUIDepAnalysis/ContentView.swift
+++ SwiftUIDepAnalysis/ContentView.swift
@@ -8,14 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var model = Model()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .background(.randomColor)
+                .debugPrint("Image evaluated")
+            VStack {
+                Text(model.sv1text)
+                Button("Increment SV1") {
+                    model.updateSV1()
+                }
+                .background(.randomColor)
+                .debugPrint("SV1 VStack evaluated")
+            }
+            VStack {
+                Text(model.sv2text)
+                Button("Increment SV2") {
+                    model.updateSV2()
+                }
+                .background(.randomColor)
+                .debugPrint("SV2 VStack evaluated")
+            }
+            VStack {
+                Text(model.sv3text)
+                Button("Increment SV3") {
+                    model.updateSV3()
+                }
+                .background(.randomColor)
+                .debugPrint("SV3 VStack evaluated")
+            }
+            VStack {
+                Text(model.sv4text)
+                Button("Increment SV4") {
+                    model.updateSV4()
+                }
+                .background(.randomColor)
+                .debugPrint("SV4 VStack evaluated")
+            }
+
         }
+        .debugPrint("body evaluated")
+        .background(.randomColor)
         .padding()
+    }
+}
+
+extension ShapeStyle where Self == Color {
+    public static var randomColor: some ShapeStyle {
+        Color(
+            red: Double.random(in: 0.0 ... 1.0),
+            green: Double.random(in: 0.0 ... 1.0),
+            blue: Double.random(in: 0.0 ... 1.0),
+            opacity: Double.random(in: 0.0 ... 1.0)
+            )
+    }
+}
+
+extension View {
+    func debugPrint(_ vars: Any...) -> some View {
+        for v in vars { print(v) }
+        return self
+    }
+    
+    func debugPrint() -> some View {
+        print(Self.self)
+        return self
+    }
+    
+    func debugPrint(_ string: String) -> some View {
+        print(string)
+        return self
+    }
+}
+
+final class Model: ObservableObject {
+    @Published var sv1text = "SV1"
+    @Published var sv2text = "SV2"
+    @Published var sv3text = "SV3"
+    @Published var sv4text = "SV4"
+    
+    private var sv1Value: Int = 0
+    private var sv2Value: Int = 0
+    private var sv3Value: Int = 0
+    private var sv4Value: Int = 0
+    
+    func updateSV1() {
+        sv1Value += 1
+        sv1text = "SV1: \(sv1Value)"
+    }
+    
+    func updateSV2() {
+        sv2Value += 1
+        sv2text = "SV2: \(sv2Value)"
+    }
+    
+    func updateSV3() {
+        sv3Value += 1
+        sv3text = "SV3: \(sv3Value)"
+    }
+    
+    func updateSV4() {
+        sv4Value += 1
+        sv4text = "SV4: \(sv4Value)"
     }
 }
 
